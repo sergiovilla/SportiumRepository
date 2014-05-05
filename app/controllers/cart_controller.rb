b9c43158a70a0b86eb00f6cab868c664386791a1
Sergio <sergiovb.inf@gmail.com>
2014-05-05 21:25:54 +0200

    Ajax de carrito terminado

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -25,23 +25,36 @@ class CartController < ApplicationController
     @ropa = Ropa.find params[:id]
     @page_title = 'Eliminar producto'
     
-    if request.post?
-      @item = @cart.remove params[:id]
-      flash[:cart_notice] = "Eliminar 1 <em>#{@item.ropa.nombre}</em>.".html_safe
-      redirect_to :controller => 'catalog'
-    else
-      render :controller => 'cart', :action => 'remove', :template => 'cart/remove'
+    respond_to do |format|
+      format.html {
+        if request.post?
+          @item = @cart.remove params[:id]
+          flash[:cart_notice] = "Eliminar 1 <em>#{@item.ropa.nombre}</em>.".html_safe
+          redirect_to :controller => 'catalog'
+        end
+      }
+      format.js {
+        @item = @cart.remove params[:id]
+        flash[:cart_notice] = "Eliminar 1 <em>#{@item.ropa.nombre}</em>.".html_safe
+      }
     end
   end
 
   def clear
-    @page_title = 'Clear cart'
-    if request.post?
-      @cart.cart_items.destroy_all
-      flash[:cart_notice] = "Carrito limpio."
-      redirect_to :controller => 'catalog'
-    else
-      render :controller => 'cart', :action => 'clear', :template => 'cart/clear'
+    @page_title = 'Vaciar Carrito'
+
+    respond_to do |format|
+      format.html {
+        if request.post?
+          @cart.cart_items.destroy_all
+          flash[:cart_notice] = "Carrito limpio."
+          redirect_to :controller => 'catalog'
+        end
+      }
+      format.js {
+        @cart.cart_items.destroy_all
+        flash[:cart_notice] = "Carrito limpio."
+      }
     end
   end
 end
