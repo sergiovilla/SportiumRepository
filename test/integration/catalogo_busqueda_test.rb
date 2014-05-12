85ab03c376c8cc3c56d4000127e356aab266d913
ruben <ruben@ruben-HP-G62-Notebook-PC.(none)>
2014-05-12 18:15:25 +0200

    cambios en catalago busqueda_test con conflictos

--- test/integration/catalogo_busqueda_test.rb
+++ test/integration/catalogo_busqueda_test.rb
@@ -1,4 +1,18 @@
 require 'test_helper'
 
 class CatalogoBusquedaTest < ActionDispatch::IntegrationTest
+
+  def latest_ropa
+    get 'catalog/latest'
+    assert_response :success
+    assert_template 'catalog/latest'
+    assert_tag :tag => 'dl', :attributes => { :id => 'ropa' },
+               :children => { :count => 5, :only => { :tag => 'dt' } }
+    @ropa = ropa.latest(5)
+    @ropa.each do |a|
+      assert_tag :tag => 'dt', :content => a.title
+    end
+  end
+
+
 end
