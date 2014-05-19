74f07ff91dc3d1e97ea5a334511f565a742003e0
Sergio <sergiovb.inf@gmail.com>
2014-05-19 16:40:51 +0200

    Añadido ajax al borrado de comentarios

--- app/controllers/foro/comentarios_controller.rb
+++ app/controllers/foro/comentarios_controller.rb
@@ -5,7 +5,12 @@ class Foro::ComentariosController < ApplicationController
     @comentario = Comentario.find(params[:id_comment])
     @comentario.destroy
 
-    flash[:notice] = "Comentario eliminado con éxito"
-    redirect_to foro_post_path(params[:id])
+    respond_to do |format|
+      format.html {
+      	flash[:notice] = "Comentario eliminado con éxito"
+        redirect_to foro_post_path(params[:id])
+      }
+      format.js
+    end
   end
 end
